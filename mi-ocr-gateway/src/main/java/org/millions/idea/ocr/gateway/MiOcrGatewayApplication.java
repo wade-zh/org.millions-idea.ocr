efd1edb47ba68417a1680bb1b02dd2f4b2cc5ccf
root <2407439@qq.com>
2018-06-08 19:03:18 +0800

    Add the rate limit strategy :cop:

--- mi-ocr-gateway/src/main/java/org/millions/idea/ocr/gateway/MiOcrGatewayApplication.java
+++ mi-ocr-gateway/src/main/java/org/millions/idea/ocr/gateway/MiOcrGatewayApplication.java
@@ -1,10 +1,13 @@
 package org.millions.idea.ocr.gateway;
 
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.DefaultRateLimiterErrorHandler;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.RateLimiterErrorHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
