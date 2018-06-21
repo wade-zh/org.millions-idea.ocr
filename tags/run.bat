284cb84ae02cd781b3c7f6ac948f5b2e6afc7845
root <2407439@qq.com>
2018-06-21 12:58:31 +0800

    There are 1 errors in the "run.bat" file

--- tags/run.bat
+++ tags/run.bat
@@ -16,25 +16,25 @@ cd ./mi-ocr-eureka-server
 call mi-ocr-eureka-server-install.bat
 call mi-ocr-eureka-server-run.bat
 
-Wscript sleep30.vbs
+
 
 cd ../
-
+Wscript sleep30.vbs
 cd ./mi-ocr-gateway
 call mi-ocr-gateway-install.bat
 call mi-ocr-gateway-run.bat
 
-Wscript sleep20.vbs
+
 
 cd ../
-
+Wscript sleep20.vbs
 cd ./mi-ocr-web-captcha-service
 call mi-ocr-web-captcha-service-install.bat
 call mi-ocr-web-captcha-service-run.bat
 
-Wscript sleep10.vbs
+
 
 cd ../
-
+Wscript sleep10.vbs
 cd ./mi-ocr-worker-win-app
 call mi-ocr-worker-win-app-run.bat
