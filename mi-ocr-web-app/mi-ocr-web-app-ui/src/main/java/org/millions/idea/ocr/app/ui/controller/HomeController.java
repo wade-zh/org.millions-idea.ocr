5941d848d7ce80f8b0d4a770aeee70cf867ff711
root <2407439@qq.com>
2018-07-11 22:32:45 +0800

    Add website static pages

---
+++ mi-ocr-web-app/mi-ocr-web-app-ui/src/main/java/org/millions/idea/ocr/app/ui/controller/HomeController.java
@@ -0,0 +1,20 @@
+/***
+ * @pName mi-ocr-web-app
+ * @name HomeController
+ * @user HongWei
+ * @date 2018/7/11
+ * @desc
+ */
+package org.millions.idea.ocr.app.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/","/index"})
+    public String index(){
+        return "un_login/index";
+    }
+}
