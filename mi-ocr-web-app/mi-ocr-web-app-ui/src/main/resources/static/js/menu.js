71141b3c0eff7891c6f082a2ef90d23c865a5058
root <2407439@qq.com>
2018-07-13 23:18:12 +0800

    Add "echarts" layout

--- mi-ocr-web-app/mi-ocr-web-app-ui/src/main/resources/static/js/menu.js
+++ mi-ocr-web-app/mi-ocr-web-app-ui/src/main/resources/static/js/menu.js
@@ -23,11 +23,7 @@ function resizeFrameHeight() {
     $('.tab_iframe').css('height', document.documentElement.clientHeight - 118);
     $('md-tab-content').css('left', '0');
 }
-window.onresize = function() {
-    resizeFrameHeight();
-    initScrollShow();
-    initScrollState();
-}
+
 
 // ========== 选项卡操作 ==========
 $(function() {
