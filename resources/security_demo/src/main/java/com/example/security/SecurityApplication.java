71bceb434d4579861a492be0477634ceeadac6d2
root <2407439@qq.com>
2018-07-19 19:18:55 +0800

    Set up the login page using custom and configurable rules

--- resources/security_demo/src/main/java/com/example/security/SecurityApplication.java
+++ resources/security_demo/src/main/java/com/example/security/SecurityApplication.java
@@ -1,13 +1,16 @@
 package com.example.security;
 
+import com.example.security.core.BrowserProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableAutoConfiguration
+@EnableConfigurationProperties(BrowserProperties.class)
 public class SecurityApplication {
 
 	public static void main(String[] args) {
--- resources/security_demo/src/main/java/com/example/security/config/BrowserSecurityConfig.java
+++ resources/security_demo/src/main/java/com/example/security/config/BrowserSecurityConfig.java
@@ -7,28 +7,41 @@
  */
 package com.example.security.config;
 
+import com.example.security.core.BrowserProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 /**
  * 应用安全适配器配置
  */
+@Component
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private BrowserProperties properties;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 验证表单
         http.formLogin()
+                .loginPage("/authentication/require")
+                .loginProcessingUrl("/authentication/form") //使用自定义的登录api
                 .and()
                 .authorizeRequests()    //下面都是需要授权的配置
+                .antMatchers("/authentication/require"
+                        ,properties.getBrowser())
+                .permitAll()    // 再此之后的所有请求将需要验证权限
                 .anyRequest()   //任何请求
-                .authenticated();   //都需要身份认证
+                .authenticated()
+                .and()
+                .csrf().disable();   //都需要身份认证
     }
 
     @Bean
