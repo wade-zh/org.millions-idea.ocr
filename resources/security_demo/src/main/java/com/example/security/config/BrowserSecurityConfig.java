eb2b3f7fcdfa63c364d683df407e92c8de5acc30
root <2407439@qq.com>
2018-07-19 17:12:18 +0800

    After 5.0, it does not support unsalted plaintext passwords

--- resources/security_demo/src/main/java/com/example/security/config/BrowserSecurityConfig.java
+++ resources/security_demo/src/main/java/com/example/security/config/BrowserSecurityConfig.java
@@ -7,10 +7,13 @@
  */
 package com.example.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * 应用安全适配器配置
@@ -26,5 +29,10 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()    //下面都是需要授权的配置
                 .anyRequest()   //任何请求
                 .authenticated();   //都需要身份认证
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
