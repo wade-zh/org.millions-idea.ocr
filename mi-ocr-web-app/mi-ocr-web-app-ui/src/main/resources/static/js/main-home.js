b6d6e7efc5fb19ffe3f719df355f2337d9a5c7d4
root <2407439@qq.com>
2018-07-27 21:06:20 +0800

    Some style resources

---
+++ mi-ocr-web-app/mi-ocr-web-app-ui/src/main/resources/static/js/main-home.js
@@ -0,0 +1,19 @@
+require.config({
+    'baseUrl': '/js/',
+    'shim': {
+        'layui': {
+            'deps': ['jquery','echarts'],
+            'exports': 'layui'
+        }
+    },'paths': {
+        'jquery': './jquery-3.3.1.min',
+        'request': './module/request',
+        'layui': '../layui/layui',
+        'carousel': './module/carousel'
+    },
+    'urlArgs': 'r=' + (new Date()).getTime()
+});
+
+require(['jquery','request','layui','carousel'],function ($, request, layui, carousel) {
+
+})
---
+++ mi-ocr-web-app/mi-ocr-web-app-ui/src/main/resources/static/js/main-user.js
@@ -0,0 +1,20 @@
+require.config({
+    'baseUrl': '/js/',
+    'shim': {
+        'layui': {
+            'deps': ['jquery','echarts'],
+            'exports': 'layui'
+        }
+    },'paths': {
+        'jquery': './jquery-3.3.1.min',
+        'request': './module/request',
+        'layui': '../layui/layui',
+        'alert': './components/alert',
+        'user': './module/user'
+    },
+    'urlArgs': 'r=' + (new Date()).getTime()
+});
+
+require(['jquery','request','layui','alert','user'],function ($, request, layui, alert, user) {
+
+})
